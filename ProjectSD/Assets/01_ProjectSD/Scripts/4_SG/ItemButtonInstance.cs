fb8a78d22d8f0eb6dac891a4deedfa7b6924d644
SG <dad0221@naver.com>
2023-10-13 14:23:05 +0900

    [CSG]feat : New Shop
    fix : ShopButtons

---
+++ ProjectSD/Assets/01_ProjectSD/Scripts/4_SG/ItemButtonInstance.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemButtonInstance : MonoBehaviour
+{
+
+    //! 상점에서 살수있는 아이템의 열값을 구해서 열의 갯수에 따라서 상점의 버튼을 생성시키는 클레스
+
+
+
+
+}
