6ebb00ad2e271647faf85a7f4bab002142f8f848
Jinseong_NB2 <sooyoung2762@gmail.com>
2023-10-16 16:34:42 +0900

    [Choi] Update: Enemy Spawn

---
+++ ProjectSD/Assets/01_ProjectSD/Scripts/3_JS/DrawCircles.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircles : MonoBehaviour
+{
+    public GameObject player; // 플레이어 오브젝트
+    public int numPoints = 6; // 원을 나눌 점의 수
+    public float circleRadius = 100f; // 원의 반지름
+    public float startAngle = 0f; // 시작 각도
+    public Material lineMaterial; // 라인 렌더러에 사용할 머티리얼
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = lineMaterial;
+        lineRenderer.positionCount = numPoints;
+    }
+
+    void Update()
+    {
+        DrawCircle();
+    }
+
+    void DrawCircle()
+    {
+        for (int i = 0; i < numPoints; i++)
+        {
+            float angle = startAngle + i * (180f / (numPoints - 1));
+            float x = player.transform.position.x + circleRadius * Mathf.Cos(Mathf.Deg2Rad * angle);
+            float z = player.transform.position.z + circleRadius * Mathf.Sin(Mathf.Deg2Rad * angle);
+            float playerPosZ = player.transform.position.z * 2;
+            Vector3 point = new Vector3(x, player.transform.position.y, -z + playerPosZ);
+            lineRenderer.SetPosition(i, point);
+
+            // 각 꼭짓점의 위치 디버그 출력
+            Debug.Log("Point " + i + " position: " + point);
+        }
+    }
+}
---
+++ ProjectSD/Assets/01_ProjectSD/Scripts/3_JS/DrawRadius.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawRadius
+{
+    private const int NUM_POINTS = 7; // 원을 나눌 점의 수
+    private const float START_ANGLE = 0f; // 시작 각도
+
+    // 원의 반지름 포지션을 저장하는 변수
+    private List<Vector3> circlePositions = new List<Vector3>();
+
+    // 함수를 호출하는 생성자
+    public DrawRadius(float radius, Vector3 playerPos)
+    {
+        // 원을 그리는 함수 실행
+        RunDrawRadius(radius, playerPos);
+    }
+
+    // 원을 그리는 함수
+    private void RunDrawRadius(float radius, Vector3 playerPos)
+    {
+        for (int i = 0; i < NUM_POINTS; i++)
+        {
+            float angle = START_ANGLE + i * (180f / (NUM_POINTS - 1));
+            float x = playerPos.x + radius * Mathf.Cos(Mathf.Deg2Rad * angle);
+            float z = playerPos.z + radius * Mathf.Sin(Mathf.Deg2Rad * angle);
+            float playerPosZ = playerPos.z * 2f;
+            Vector3 point = new Vector3(x, playerPos.y, -z + playerPosZ);
+
+            // 각 꼭짓점의 위치 디버그 출력
+            Debug.Log("Point " + i + " position: " + point);
+
+            // 반지름 포지션 추가
+            circlePositions.Add(point);
+        }
+    }
+
+    // 저장된 반지름 포지션 리스트를 반환하는 함수
+    public List<Vector3> GetCirclePositions()
+    {
+        return circlePositions;
+    }
+}
