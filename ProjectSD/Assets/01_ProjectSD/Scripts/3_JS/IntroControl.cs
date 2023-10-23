315991114011d0d5ea62daa7e95f2206da769d24
Jinseong_NB2 <sooyoung2762@gmail.com>
2023-10-23 17:06:25 +0900

    [Choi] Vibration Test

--- ProjectSD/Assets/01_ProjectSD/Scripts/3_JS/IntroControl.cs
+++ ProjectSD/Assets/01_ProjectSD/Scripts/3_JS/IntroControl.cs
@@ -9,22 +9,35 @@ public class IntroControl : MonoBehaviour
     public TextMeshProUGUI introText;
     private string[] dialogue =
     {
-        "평화롭던 어느 날… 세계수를 노리고 거대 골렘이 다가오기 시작했다. \n" +
-            "골렘을 막을 수 있는 이는 단 한 사람.",
+        "평화롭던 어느 날… 세계수를 노리고 거대 골렘이 \n 다가오기 시작했다.",
+        "골렘을 막을 수 있는 이는 단 한 사람.",
         "그는 바로...",
         "돌펜스! 돌펜스!",
+        ""
     };
-    private float delay = 3.0f;
+    private float[] delay =
+    {
+        7.0f, 4.0f, 3.0f, 2.0f, 2.0f
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 메세지 출력 코루틴 실행
+        StartCoroutine(PrintDialogueCoroutine(delay));
     }
 
-    private IEnumerator PrintDialogueCoroutine(float t)
+    // 다이얼로그 메세지를 출력하는 코루틴 함수
+    private IEnumerator PrintDialogueCoroutine(float[] t)
     {
-        // 대기
-        yield return new WaitForSeconds(t);
+        for (int i = 0; i < t.Length; i++)
+        {
+            // 대기
+            yield return new WaitForSeconds(t[i]);
+
+            // 텍스트 변경
+            introText.text = dialogue[i];
+        }
+
     }
 }
